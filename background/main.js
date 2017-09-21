6ee0da089eb5bffc7fbe046f58cc2c0f0e612c4d
mbnuqw <maxbadryzlov@gmail.com>
2017-09-22 00:37:08 +0500

    Init

---
+++ background/main.js
@@ -0,0 +1,31 @@
+const WasmExtRGX = /\.wasm$/i
+const Tabs = []
+
+/**
+*  Listen requests
+**/
+window.browser.webRequest.onBeforeRequest.addListener(
+  req => {
+    if (req.type === 'main_frame') {
+      if (Tabs.indexOf(req.tabId) !== -1)
+        window.browser.pageAction.hide(req.tabId)
+      return
+    }
+
+    if (WasmExtRGX.test(req.url)) {
+      if (Tabs.indexOf(req.tabId) === -1) Tabs.push(req.tabId)
+      window.browser.pageAction.show(req.tabId)
+      window.browser.pageAction.setTitle({ tabId: req.tabId, title: req.url })
+    }
+  },
+  {
+    urls: ['<all_urls>'],
+  }
+)
+
+/**
+*  Remove listed tab
+**/
+window.browser.tabs.onRemoved.addListener(tabId => {
+  if (Tabs.indexOf(tabId) !== -1) Tabs.splice(Tabs.indexOf(tabId), 1)
+})
